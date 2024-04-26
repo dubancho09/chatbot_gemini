5af3294d01706b132ee67f25ccec261a9905c846
duban0909 <duban5555@gmail.com>
2024-04-25 21:22:22 -0500

    Configuration envs and create the enter point of application

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hola mundo");
+import { envs } from "./config/envs.adapter";
+import { Server } from "./presentation/server";
+
+
+(async () => {
+    main();
+})();
+
+
+async function main(){
+
+
+    const server = new Server({
+        ia_api_key: envs.GEMINI_API_KEY
+    });
+
+    server.start();
+}
